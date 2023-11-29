957ed973e49d187dce99741a250443e96aed00a1
Fletcher <fletcher.boyd@artifactory.org.au>
2023-11-29 16:35:51 +0800

    Black formatting

--- project_output.py
+++ project_output.py
@@ -40,7 +40,9 @@ def lookup(id):
 def send_invoices(p):
     if p.get("dgr", False):
         title_prefix = "Gift/Donation for: "
-        message_suffix = "\nAs a reminder your donation to this project is tax deductible."
+        message_suffix = (
+            "\nAs a reminder your donation to this project is tax deductible."
+        )
         admin_suffix = "\nThese invoices have been marked as tax deductible."
         category = config["tidyhq_dgr_category"]
     else:
@@ -48,10 +50,10 @@ def send_invoices(p):
         message_suffix = ""
         admin_suffix = "\nThese invoices have **not** been marked as tax deductible."
         category = config["tidyhq_project_category"]
-        
+
     admin_notifaction = f'Invoices for {p["title"]} have been created: '
     sent_total = 0
-    
+
     for pledge in p["pledges"]:
         amount = p["pledges"][pledge]
         r = requests.post(
@@ -70,46 +72,46 @@ def send_invoices(p):
             },
         )
         invoice = r.json()
-        print(f'${invoice["amount"]} invoice created for {users[pledge][0]} (https://{domain}.tidyhq.com/finances/invoices/{invoice["id"]}))')
+        print(
+            f'${invoice["amount"]} invoice created for {users[pledge][0]} (https://{domain}.tidyhq.com/finances/invoices/{invoice["id"]}))'
+        )
         admin_notifaction += f'\n* ${invoice["amount"]} for <@{users[pledge][1]}> - <https://{domain}.tidyhq.com/finances/invoices/{invoice["id"]}|{invoice["id"]}>'
         sent_total += invoice["amount"]
-        
+
         # Open a slack conversation with the donor and get the channel ID
         r = app.client.conversations_open(users=pledge)
         channel_id = r["channel"]["id"]
-        
+
         # Send a message to the donor to let them know an invoice has been created
         app.client.chat_postMessage(
             channel=channel_id,
-            text=f'The funding goal for {p["title"]} has been met. I\'ve created an invoice for ${amount} which you can find <https://{domain}.tidyhq.com/public/invoices/{invoice["id"]}|here>.{message_suffix}'
+            text=f'The funding goal for {p["title"]} has been met. I\'ve created an invoice for ${amount} which you can find <https://{domain}.tidyhq.com/public/invoices/{invoice["id"]}|here>.{message_suffix}',
         )
-        
-        print(f'Invoice notification sent to {users[pledge][0]}')
-    
+
+        print(f"Invoice notification sent to {users[pledge][0]}")
+
     # Open a slack conversation with the project creator and get the channel ID
     r = app.client.conversations_open(users=p["created by"])
     channel_id = r["channel"]["id"]
-        
+
     # Send a message to the project creator to let them know the invoices have been created
     app.client.chat_postMessage(
         channel=channel_id,
-        text=f'The funding goal for a project you created ({p["title"]}) has been met and invoices have been sent out. Please contact the Treasurer for the next steps.'
-    )    
-    
-    print(f'Invoice notification sent to {users[p["created by"]][0]} as project creator')
-    
+        text=f'The funding goal for a project you created ({p["title"]}) has been met and invoices have been sent out. Please contact the Treasurer for the next steps.',
+    )
+
+    print(
+        f'Invoice notification sent to {users[p["created by"]][0]} as project creator'
+    )
+
     # Send invoice creation details to the admin channel
-    
+
     admin_notifaction += f'\n\nProject goal: ${p["total"]}'
-    admin_notifaction += f'\nTotal sent: ${sent_total}'
+    admin_notifaction += f"\nTotal sent: ${sent_total}"
     admin_notifaction += admin_suffix
     admin_notifaction += f'\n\nA notification has also been sent to <@{p["created by"]}> as the project creator. They\'ve been asked to contact the Treasurer for the next steps.'
-    
-    
-    app.client.chat_postMessage(
-        channel=config["admin_channel"],
-        text=admin_notifaction
-    )
+
+    app.client.chat_postMessage(channel=config["admin_channel"], text=admin_notifaction)
 
 
 # Initialise slack
@@ -117,7 +119,7 @@ def send_invoices(p):
 app = App(token=config["SLACK_BOT_TOKEN"])
 
 # Populate users from file
-try: 
+try:
     with open("tidyslack.json", "r") as f:
         users = json.load(f)
 except FileNotFoundError:
@@ -141,7 +143,9 @@ for contact in r.json():
         ):
             r = app.client.users_info(user=field["value"])
             users[field["value"]] = (
-                r["user"].get("real_name", r["user"].get("display_name", r["user"]["name"])),
+                r["user"].get(
+                    "real_name", r["user"].get("display_name", r["user"]["name"])
+                ),
                 r["user"]["name"],
                 contact["contact_id"],
             )
@@ -159,7 +163,7 @@ r = requests.get(
     params={"access_token": config["tidyhq_token"]},
 )
 domain = r.json()["domain_prefix"]
-print(f'Domain is {domain}.tidyhq.com')
+print(f"Domain is {domain}.tidyhq.com")
 
 for project in projects:
     p = projects[project]
