86b650d0ffcaf13466a69c25843a2e9a0ca99489
Fletcher <fletcher.boyd@artifactory.org.au>
2024-03-15 18:45:25 +0800

    Patch bug in invoice display

--- utils/project_output.py
+++ utils/project_output.py
@@ -32,25 +32,29 @@ def lookup(id: str) -> tuple[str, str, int]:
 
 
 def send_invoices(p: dict[str, Any], module: bool = False) -> None:
-    
+
     # Check if project has already been processed
     if "invoices_sent" in p.keys():
         # Date formatting for slack
         invoice_sent_str = f'<!date^{p["invoices_sent"]}^Sent {{date_pretty}}|Sent {str(datetime.fromtimestamp(p["invoices_sent"]))}>'
-        
+
         print("Invoices already sent, skipped")
-        return f'Error: Invoices for this project were sent on {invoice_sent_str}.'
-    
+        return f"Error: Invoices for this project were sent on {invoice_sent_str}."
+
     # Check if every donor has a tidyhq ID, this is only really a problem when running as a module
     if module:
         for pledge in p["pledges"]:
             if pledge not in members.keys():
-                return f'Error: <@{pledge}> does not have a Slack ID associated with their TidyHQ account. Update the field within TidyHQ and try again.'
-    
+                return f"Error: <@{pledge}> does not have a Slack ID associated with their TidyHQ account. Update the field within TidyHQ and try again."
+
     if p.get("dgr", False):
         title_prefix = "Gift/Donation for: "
-        message_suffix: str = f'\nAs a reminder your donation to this project is <{config["tax_info"]}|tax deductible>.'
-        admin_suffix: str = f'\nThese invoices have been marked as <{config["tax_info"]}|tax deductible>.'
+        message_suffix: str = (
+            f'\nAs a reminder your donation to this project is <{config["tax_info"]}|tax deductible>.'
+        )
+        admin_suffix: str = (
+            f'\nThese invoices have been marked as <{config["tax_info"]}|tax deductible>.'
+        )
         category: int = int(config["tidyhq_dgr_category"])
     else:
         title_prefix = "Project pledge: "
@@ -64,45 +68,45 @@ def send_invoices(p: dict[str, Any], module: bool = False) -> None:
     for pledge in p["pledges"]:
         amount: int = p["pledges"][pledge]
         details: dict[str, Any] = {
-                "access_token": str(config["tidyhq_token"]),
-                "reference": str(p["title"]),
-                "name": str(title_prefix + p["title"]),
-                "amount": amount,
-                "included_tax_total": amount,
-                "pre_tax_amount": amount,
-                "due_date": datetime.now() + timedelta(days=14),
-                "category_id": category,
-                "contact_id": int(members[pledge][2]),
-                "metadata": "Automatically added via api",
-            }
+            "access_token": str(config["tidyhq_token"]),
+            "reference": str(p["title"]),
+            "name": str(title_prefix + p["title"]),
+            "amount": amount,
+            "included_tax_total": amount,
+            "pre_tax_amount": amount,
+            "due_date": datetime.now() + timedelta(days=14),
+            "category_id": category,
+            "contact_id": int(members[pledge][2]),
+            "metadata": "Automatically added via api",
+        }
 
-        invoice: dict[str,Any] = requests.post(
-            "https://api.tidyhq.com/v1/invoices/",
-            params=details).json()
+        invoice: dict[str, Any] = requests.post(
+            "https://api.tidyhq.com/v1/invoices/", params=details
+        ).json()
         print(
-            f'${invoice["amount"]} invoice created for {members[pledge][0]} (https://{domain}.tidyhq.com/finances/invoices/{invoice["id"]}))'
+            f'${invoice.get("amount","?")} invoice created for {members[pledge][0]} (https://{domain}.tidyhq.com/finances/invoices/{invoice["id"]}))'
         )
-        admin_notification += f'\n• ${invoice["amount"]} for <@{members[pledge][1]}> - <https://{domain}.tidyhq.com/finances/invoices/{invoice["id"]}|{invoice["id"]}>'
+        admin_notification += f'\n• ${invoice.get("amount","?")} for <@{members[pledge][1]}> - <https://{domain}.tidyhq.com/finances/invoices/{invoice["id"]}|{invoice["id"]}>'
         sent_total += int(invoice["amount"])
 
         # Open a slack conversation with the donor and get the channel ID
-        r: SlackResponse = invoice_slack_app.client.conversations_open(users=pledge) # type: ignore
-        channel_id = str(r["channel"]["id"]) # type: ignore
+        r: SlackResponse = invoice_slack_app.client.conversations_open(users=pledge)  # type: ignore
+        channel_id = str(r["channel"]["id"])  # type: ignore
 
         # Send a message to the donor to let them know an invoice has been created
-        invoice_slack_app.client.chat_postMessage( # type: ignore
-            channel=channel_id, # type: ignore
+        invoice_slack_app.client.chat_postMessage(  # type: ignore
+            channel=channel_id,  # type: ignore
             text=f'The funding goal for {p["title"]} has been met. I\'ve created an invoice for ${amount} which you can find <https://{domain}.tidyhq.com/public/invoices/{invoice["id"]}|here>.{message_suffix}',
         )
 
         print(f"Invoice notification sent to {members[pledge][0]}")
 
     # Open a slack conversation with the project creator and get the channel ID
-    r = invoice_slack_app.client.conversations_open(users=p["created by"]) # type: ignore
-    channel_id: str = r["channel"]["id"] # type: ignore
+    r = invoice_slack_app.client.conversations_open(users=p["created by"])  # type: ignore
+    channel_id: str = r["channel"]["id"]  # type: ignore
 
     # Send a message to the project creator to let them know the invoices have been created
-    invoice_slack_app.client.chat_postMessage( # type: ignore
+    invoice_slack_app.client.chat_postMessage(  # type: ignore
         channel=channel_id,
         text=f'The funding goal for a project you created ({p["title"]}) has been met and invoices have been sent out. Please contact the Treasurer for the next steps.',
     )
@@ -121,16 +125,18 @@ def send_invoices(p: dict[str, Any], module: bool = False) -> None:
     # If we're running as a module, return the admin notification rather than posting it to slack
     if module:
         return admin_notification
-    
-    invoice_slack_app.client.chat_postMessage(channel=str(config["admin_channel"]), text=admin_notification) # type: ignore
-    
+
+    invoice_slack_app.client.chat_postMessage(channel=str(config["admin_channel"]), text=admin_notification)  # type: ignore
+
     return "Success: Invoices sent."
 
-def loadConfig() -> dict[str,str|int]:
+
+def loadConfig() -> dict[str, str | int]:
     with open("config.json", "r") as f:
         return json.load(f)
 
-def updateUsers() -> dict[str,tuple[str,str,int]]:
+
+def updateUsers() -> dict[str, tuple[str, str, int]]:
     global members
     # Populate users from file
     try:
@@ -141,7 +147,7 @@ def updateUsers() -> dict[str,tuple[str,str,int]]:
 
     print("Pulling TidyHQ contacts...")
 
-    contacts: list[dict[str,Any]] = requests.get(
+    contacts: list[dict[str, Any]] = requests.get(
         "https://api.tidyhq.com/v1/contacts/",
         params={"access_token": config["tidyhq_token"]},
     ).json()
@@ -154,9 +160,11 @@ def updateUsers() -> dict[str,tuple[str,str,int]]:
             if (
                 field["id"] == config["tidyhq_slack_id_field"]
                 and field["value"] not in members.keys()
-                and field["value"] 
+                and field["value"]
             ):
-                r: SlackResponse = invoice_slack_app.client.users_info(user=field["value"])
+                r: SlackResponse = invoice_slack_app.client.users_info(
+                    user=field["value"]
+                )
                 members[field["value"]] = (
                     r["user"].get(
                         "real_name", r["user"].get("display_name", r["user"]["name"])
@@ -169,31 +177,33 @@ def updateUsers() -> dict[str,tuple[str,str,int]]:
     with open("tidyslack.json", "w") as f:
         json.dump(members, f, indent=4, sort_keys=True)
 
+
 def send_invoices_lib(id: str) -> str:
     # Load projects
     projects = loadProjects()
-    
+
     # Check if project exists
-    
+
     if id not in projects.keys():
         raise Exception("Project not found")
-    
+
     # Initialise slack
     global invoice_slack_app
     invoice_slack_app = App(token=str(config["SLACK_BOT_TOKEN"]))
 
     # Get users
     updateUsers()
-    
+
     outcome = send_invoices(projects[id], module=True)
-    
+
     # Update projects.json if invoices were sent successfully
     if outcome.startswith("Success"):
         projects[id]["invoices_sent"] = int(datetime.now().timestamp())
         with open("projects.json", "w") as f:
             json.dump(projects, f, indent=4, sort_keys=True)
-    
+
     return outcome
+
 
 # Get info that isn't expected to change while the script is running.
 
@@ -216,7 +226,7 @@ if __name__ == "__main__":
     projects = loadProjects()
 
     for project in projects:
-        p: dict[str,Any] = projects[project]
+        p: dict[str, Any] = projects[project]
         print(
             f'{p["title"]} - created by {lookup(p["created by"])[0]} (@{lookup(p["created by"])[1]})'
         )
